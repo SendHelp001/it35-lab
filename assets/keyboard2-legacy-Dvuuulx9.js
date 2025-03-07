dff528e0802baa7e8e6f7ecfc68aaf42ffb83e19
SendHelp001 <jonardsalanip@gmail.com>
2025-03-07 11:03:44 +0800

    Updates

--- assets/keyboard2-legacy-Dvuuulx9.js
+++ assets/keyboard2-legacy-Dvuuulx9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BRbdOhJi.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-_cP9Nsbb.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
