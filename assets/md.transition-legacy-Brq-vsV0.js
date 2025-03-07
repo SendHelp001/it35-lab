04615925cbe668cd64743fe67268d3558eff30ce
SendHelp001 <jonardsalanip@gmail.com>
2025-03-07 12:16:24 +0800

    Updates

--- assets/md.transition-legacy-Brq-vsV0.js
+++ assets/md.transition-legacy-Brq-vsV0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-_cP9Nsbb.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-tv9CUjql.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Bnfpvocr.js
+++ assets/swipe-back-legacy-Bnfpvocr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-_cP9Nsbb.js","./index3-legacy-D05I-ow7.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-tv9CUjql.js","./index3-legacy-CwZ36ChU.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
