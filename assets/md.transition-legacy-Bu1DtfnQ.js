97e6339feb81f1a4e5b230e46b9a9a06bda6e0b8
SendHelp001 <jonardsalanip@gmail.com>
2025-04-28 00:12:36 +0800

    Updates

--- assets/md.transition-legacy-Bu1DtfnQ.js
+++ assets/md.transition-legacy-Bu1DtfnQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D1Cv0djl.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CXugGxDK.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
