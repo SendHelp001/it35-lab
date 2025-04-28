436b1bf806e33babc2ea13071a3e95dc20e8b0b3
SendHelp001 <jonardsalanip@gmail.com>
2025-04-28 11:17:38 +0800

    Updates

--- assets/ios.transition-legacy-KyLUjqCH.js
+++ assets/ios.transition-legacy-KyLUjqCH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-VHBnRLvP.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-iIRQ2yoi.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DreuJWr1.js
+++ assets/swipe-back-legacy-DreuJWr1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-VHBnRLvP.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-iIRQ2yoi.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
