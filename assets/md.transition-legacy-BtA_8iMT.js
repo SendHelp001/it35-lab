a50b522baa0574faf12b97c1c25893b3b3b524c1
SendHelp001 <jonardsalanip@gmail.com>
2025-04-28 12:26:52 +0800

    Updates

--- assets/md.transition-legacy-BtA_8iMT.js
+++ assets/md.transition-legacy-BtA_8iMT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-iIRQ2yoi.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-37FxyN0h.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
